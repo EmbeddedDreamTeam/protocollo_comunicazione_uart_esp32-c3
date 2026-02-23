d4134215dd0244107ac4a5f15c5d48fa15a7cff5
cesare <cesarov04@gmail.com>
2026-02-23 11:14:31 +0100

    conversion in cpp

---
+++ src/commands.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "esp_task_wdt.h"
+
+#include "structs.h"
+
+//* _______________________________________ EXECUTE COMMANDS
+void task_execute_command_01(void *arg){
+  while(1){
+    Msg *msg = nullptr;
+    xQueueReceive(h_queue_command_01, &msg, portMAX_DELAY);
+    printf("START: execute_command_01\n");
+    vTaskDelay(pdMS_TO_TICKS(7000));
+    printf("END: execute_command_01\n");
+    delete msg; 
+  }
+}
+
+void task_execute_command_02(void *arg){
+  while(1){
+    Msg *msg = nullptr;
+    xQueueReceive(h_queue_command_02, &msg, portMAX_DELAY);
+    printf("START: execute_command_02\n");
+    vTaskDelay(pdMS_TO_TICKS(9000));
+    printf("END: execute_command_02\n");
+    delete msg; 
+  }
+}
